8015c7646bb371bacca30b323635b5e5bfeff680
Cathy D'Onofrio <catd825@gmail.com>
2020-03-15 21:18:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,18 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(b="Squawk!", *p)
+  puts b, *p
+  return "Squawk!"
+  return "Pretty bird!"
+end
+
+
+# default phrase = squawk!
+# optional phrase = pretty bird (use *)
+#
+
+
+# def parrot(b="Squawk!", *p)
+#   puts b, *p
+#   return "Squawk!", "Pretty Bird!"
+# end
